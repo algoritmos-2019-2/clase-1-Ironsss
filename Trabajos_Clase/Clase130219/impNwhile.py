fb37af544ff1b0ac743f81d8801a1c050f527c1b
Ironsss <david-alexis2010@hotmail.com>
2019-02-13 13:31:50 -0600

    Programa de impresión de números con while

---
+++ Trabajos_Clase/Clase130219/impNwhile.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+
+#García Espinosa David Alexis Algoritmos C. 2019-2
+#Programa para Imprimir n números con while 
+
+num = int(input("¿Cuál es tu número?"))
+i = 1
+while i <= num:
+   print(i)
+   i = i+1
+print("Hasta aqui llega tu cuenta")
