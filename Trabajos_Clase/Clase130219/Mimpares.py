2d54017c2d41800970dc646aa364e99e3781e406
Ironsss <david-alexis2010@hotmail.com>
2019-02-13 13:36:48 -0600

    Programa para imprimir los primeros números impares con while

---
+++ Trabajos_Clase/Clase130219/Mimpares.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+
+#García Espinosa David Alexis Algoritmos C. 2019-2
+#Programa para Imprimir m números impares
+
+num = int(input("¿Cuál es tu número?"))
+i = 1
+while i <= num:
+   print(i)
+   i = (i*2)+1
+print("Hasta aqui llega tu cuenta")
